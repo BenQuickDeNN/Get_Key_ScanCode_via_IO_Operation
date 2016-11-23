9131cba4e8f81bd7d8f0b94fc9d02582c8e1badf
Ben Quick <2413269761@qq.com>
2016-11-23 23:17:00 +0800

    Add files via upload

---
+++ IOOperation.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication_IOdemo
+{
+    class IOOperation
+    {
+        [DllImport("inpout32.dll")]
+        public static extern UInt32 IsInpOutDriverOpen();
+        [DllImport("inpout32.dll")]
+        public static extern void Out32(short PortAddress, short Data);
+        [DllImport("inpout32.dll")]
+        public static extern char Inp32(short PortAddress);
+
+        [DllImport("inpout32.dll")]
+        public static extern void DlPortWritePortUshort(short PortAddress, ushort Data);
+        [DllImport("inpout32.dll")]
+        public static extern ushort DlPortReadPortUshort(short PortAddress);
+
+        [DllImport("inpout32.dll")]
+        public static extern void DlPortWritePortUlong(int PortAddress, uint Data);
+        [DllImport("inpout32.dll")]
+        public static extern uint DlPortReadPortUlong(int PortAddress);
+
+        [DllImport("inpoutx64.dll")]
+        public static extern bool GetPhysLong(ref int PortAddress, ref uint Data);
+        [DllImport("inpoutx64.dll")]
+        public static extern bool SetPhysLong(ref int PortAddress, ref uint Data);
+
+
+        [DllImport("inpoutx64.dll", EntryPoint = "IsInpOutDriverOpen")]
+        public static extern UInt32 IsInpOutDriverOpen_x64();
+        [DllImport("inpoutx64.dll", EntryPoint = "Out32")]
+        public static extern void Out32_x64(short PortAddress, short Data);
+        [DllImport("inpoutx64.dll", EntryPoint = "Inp32")]
+        public static extern char Inp32_x64(short PortAddress);
+
+        [DllImport("inpoutx64.dll", EntryPoint = "DlPortWritePortUshort")]
+        public static extern void DlPortWritePortUshort_x64(short PortAddress, ushort Data);
+        [DllImport("inpoutx64.dll", EntryPoint = "DlPortReadPortUshort")]
+        public static extern ushort DlPortReadPortUshort_x64(short PortAddress);
+
+        [DllImport("inpoutx64.dll", EntryPoint = "DlPortWritePortUlong")]
+        public static extern void DlPortWritePortUlong_x64(int PortAddress, uint Data);
+        [DllImport("inpoutx64.dll", EntryPoint = "DlPortReadPortUlong")]
+        public static extern uint DlPortReadPortUlong_x64(int PortAddress);
+
+        [DllImport("inpoutx64.dll", EntryPoint = "GetPhysLong")]
+        public static extern bool GetPhysLong_x64(ref int PortAddress, ref uint Data);
+        [DllImport("inpoutx64.dll", EntryPoint = "SetPhysLong")]
+        public static extern bool SetPhysLong_x64(ref int PortAddress, ref uint Data);
+        public static void acessPort(short PORT_KEY)
+        {
+            // 循环显示IO端口状态
+            uint info = 0;
+            try
+            {
+                info = IOOperation.IsInpOutDriverOpen();
+                Console.WriteLine("info: " + info + "\n");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString() + "\n");
+                Console.WriteLine("info: " + info + "\n");
+                Console.WriteLine("未能打开IO驱动!\n");
+                return;
+            }
+            try
+            {
+                info = IOOperation.Inp32(PORT_KEY);
+                Console.WriteLine("PORT: " + PORT_KEY.ToString("x8") + "    BYTE: " + info.ToString("x8") + "\n");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString() + "\n");
+                Console.WriteLine("info: " + info + "\n");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication_IOdemo
+{
+    class Program
+    {
+        const int PORT_KEY = 0x0060;// 键盘输入值端口
+        static void Main(string[] args)
+        {
+            UInt16 in_value;
+            UInt16 in_value_old = '\0';
+            uint info;
+            while ((info = IOOperation.IsInpOutDriverOpen()) == 0) ;// 检查IO端口驱动是否已打开
+            Console.WriteLine("IO端口驱动已打开，按ESC键退出");
+            while (true)
+            {
+                in_value = IOOperation.Inp32(PORT_KEY);// 得到键盘扫描码
+                //in_value ^= 0xFF7F;// 取反
+                //in_value &= (char)0xFF00;// 只取8位
+                if (in_value == 0x0001) break;
+                if (in_value != in_value_old && in_value != 0x003F)
+                {
+                    Console.WriteLine("您按了扫描码为: " + in_value.ToString("x8") + " 的键");
+                }
+                in_value_old = in_value;
+                Thread.Sleep(1);
+            }
+        }
+    }
+}
